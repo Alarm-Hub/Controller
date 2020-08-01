60818a69822da0021dd8edd503f087d2b9d3fc9b
Daniel Bacher <mail@phill93.de>
2020-08-01 18:09:15 +0200

    add code model

--- code_manager/models.py
+++ code_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .validators import validate_code
+
 
 # Create your models here.
+
+class Code(models.Model):
+    code = models.PositiveIntegerField(
+        verbose_name="Code",
+        unique=True,
+        validators=[validate_code]
+    )
