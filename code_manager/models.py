b4a6cf8b7987069d0a7ab21042c3c088e5db56b9
Daniel Bacher <mail@phill93.de>
2020-08-09 17:41:16 +0200

    extend code model

--- code_manager/models.py
+++ code_manager/models.py
@@ -5,6 +5,12 @@ from .validators import validate_code
 # Create your models here.
 
 class Code(models.Model):
+    name = models.CharField(
+        verbose_name="Name",
+        max_length=250,
+        null=True
+    )
+
     code = models.PositiveIntegerField(
         verbose_name="Code",
         unique=True,
@@ -16,5 +22,24 @@ class Code(models.Model):
         null=True
     )
 
+    ALLOWED_CHOICES = [
+        ("always", "Immer"),
+        ("alarm", "Nach Alarm")
+    ]
+
+    allowed = models.CharField(
+        verbose_name="Erlaubt",
+        max_length=100,
+        choices=ALLOWED_CHOICES,
+        default=ALLOWED_CHOICES[0][0]
+    )
+
+    valid_after_alarm = models.PositiveIntegerField(
+        verbose_name="Gültig x Min nach Alarm",
+        default=10,
+        null=True,
+        blank=True
+    )
+
     def __str__(self):
-        return self.code.__str__()
+        return self.name
